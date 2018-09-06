b7f21c9c587155d9d3cd5e77d193d3b735bcda78
Damini Singh <Damini611@users.noreply.github.com>
2018-09-06 14:49:38 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <h2> Welcome to Love to Eat Application!! </h2>
+    );
+  }
+}
+
+//App is exported default
+export default App;
